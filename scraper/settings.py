6957284842c34ae3111f6f4f23a486944f1d862f
Md Zubair Beg <ziibii88@gmail.com>
2023-08-19 13:57:13 +0800

    removed items pipeline from settings

--- scraper/settings.py
+++ scraper/settings.py
@@ -72,9 +72,9 @@ DOWNLOAD_HANDLERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    "scraper.pipelines.ProxyPipeline": 300,
-}
+# ITEM_PIPELINES = {
+#     "scraper.pipelines.ProxyPipeline": 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
