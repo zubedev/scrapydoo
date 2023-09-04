c1832463de58e3529d2fbdb94fe4de1ff2a808cf
Md Zubair Beg <ziibii88@gmail.com>
2023-09-04 19:12:58 +0800

    scrape from proxyscrape api

--- scraper/spiders/proxyscrape.py
+++ scraper/spiders/proxyscrape.py
@@ -1,41 +1,68 @@
-from scrapy import Selector
+from collections.abc import Generator
+from typing import Any
+
+from scrapy.http import TextResponse
 
 from scraper.base import BaseSpider
+from scraper.items import ProxyItem
 from scraper.types import ElementPathsTypedDict
 
 
 class ProxyScrapeSpider(BaseSpider):
-    """
-    <tbody id="proxytable">
-        <tr>
-            <td>36.112.137.173</td>
-            <td>8888</td>
-            <td>HTTP</td>
-            <td>China</td>
-            <td>Anonymous</td>
-            <td class="ms-section">7651ms</td>
-            <td>44.4%</td>
-            <td>8 minutes</td>
-        </tr>
+    """https://docs.proxyscrape.com
+
+    in plain text format:
+        111.21.183.58:9091
+        78.38.93.20:3128
+        103.45.105.226:8080
         ... ... ...
-    </tbody>
     """
 
     name = "proxyscrape"
     allowed_domains = ["proxyscrape.com"]
-    start_urls = ["https://proxyscrape.com/free-proxy-list/"]
-    use_playwright = True
+
+    def __init__(self, name: str = None, **kwargs: Any) -> None:  # type: ignore[assignment]
+        super().__init__(name=name, **kwargs)
+        self.start_urls: list[str] = self.build_urls()
+
+    def build_urls(self) -> list[str]:
+        protocols = ["http", "socks4", "socks5"]
+        ssl = ["yes", "no"]
+        anonymity = ["elite", "anonymous"]
+
+        return [
+            (
+                f"https://api.proxyscrape.com/v2/?request=displayproxies&"
+                f"protocol={p}&ssl={s}&anonymity={a}&country=all&timeout=10000"
+            )
+            for a in anonymity
+            for s in ssl
+            for p in protocols
+        ]
 
     def set_element_paths(self) -> ElementPathsTypedDict:
-        return {
-            "rows": "//tbody[@id='proxytable']/tr",
-            "ip": "./td[1]/text()",
-            "port": "./td[2]/text()",
-            "protocol": "./td[3]/text()",
-            "country": "./td[4]/text()",
-            "anonymity": "./td[5]/text()",
-        }
+        return {}  # type: ignore[typeddict-item]
 
-    def parse_country(self, row: Selector) -> str:
-        country = self.get_data(row, self.paths["country"])
-        return self.match_data(country.lower(), r"[a-z ]+")
+    def parse(self, response: TextResponse, **kwargs: Any) -> Generator[dict[str, Any], Any, None]:
+        url = response.url
+        params = url.split("&")[1:]
+
+        protocol = params[0].split("=")[1]
+        ssl = params[1].split("=")[1]
+        anonymity = params[2].split("=")[1]
+
+        if protocol == "http" and ssl == "yes":
+            protocol = "https"
+
+        for line in response.text.splitlines():
+            ip, port = line.split(":")
+            ip = self.match_data(ip, self.get_pattern("ip"))
+            port = self.match_data(port, self.get_pattern("port"))
+            yield ProxyItem(
+                ip=ip,
+                port=int(port) if port else 0,
+                protocol=protocol,
+                country="",  # not available
+                anonymity=anonymity,
+                source=self.name,
+            )
